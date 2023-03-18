be2566dbc98a120ef70c2c49fa0c6b135c0684fe
wasabigeek <wasabigeek@users.noreply.github.com>
2023-03-18 11:54:44 +0800

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+  backend "gcs" {
+    bucket  = "parkcheep-tf-state-prod"
+    prefix  = "terraform/state"
+  }
+}
+
+provider "google" {
+  credentials = file("parkcheep-gcp.json")
+
+  project = "parkcheep"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "parkcheep-telegram"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+    description             = "Allow SSH from anywhere"
+    destination_ranges      = []
+    direction               = "INGRESS"
+    disabled                = false
+    name                    = "default-allow-ssh"
+    network                 = google_compute_network.vpc_network.self_link
+    priority                = 65534
+    project                 = "parkcheep"
+    source_ranges           = [
+        "0.0.0.0/0",
+    ]
+
+    allow {
+        ports    = [
+            "22",
+        ]
+        protocol = "tcp"
+    }
+}
