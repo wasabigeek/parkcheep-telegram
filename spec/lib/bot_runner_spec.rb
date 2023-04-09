3aef727310a3370444febd67c5f9c8cca1d5dfd5
wasabigeek <wasabigeek@users.noreply.github.com>
2023-04-09 22:44:50 +0800

    Extend integration test

--- spec/lib/bot_runner_spec.rb
+++ spec/lib/bot_runner_spec.rb
@@ -5,22 +5,43 @@ RSpec.describe BotRunner do
   describe "#handle_message with Telegram::Bot::Types::Message" do
     before do
       ENV["TELEGRAM_TOKEN"] = "123"
+      ENV["GOOGLE_MAPS_API_KEY"] = "456"
       Time.zone = "Asia/Singapore"
     end
-    after { ENV["TELEGRAM_TOKEN"] = nil }
-    it "enters the SearchState on /start" do
+    after do
+      ENV["TELEGRAM_TOKEN"] = nil
+      ENV["GOOGLE_MAPS_API_KEY"] = nil
+    end
+    it "integration test" do
       api = double("api", send_message: nil)
       bot = double("bot", api:)
+
+      # start
       message = Telegram::Bot::Types::Message.new(text: "/start")
       allow(message).to receive(:chat).and_return(double("chat", id: 1))
+      expect(api).to receive(:send_message).with(
+        { chat_id: 1, text: "👋 Hello! Please type your destination." }
+      )
 
       runner = BotRunner.new
       runner.handle_message(bot, message)
 
       expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(SearchState)
-      expect(api).to have_received(:send_message).with(
-        { chat_id: 1, text: "👋 Hello! Please type your destination." }
-      )
+
+      # searching, no location found
+      expect_any_instance_of(Parkcheep::Geocoder).to receive(:geocode).with("changi airport").and_return([])
+      expect(api).to receive(:send_message).with(
+        { chat_id: 1, text: "Searching for \"changi airport, Singapore\"..." }
+      ).ordered
+      expect(api).to receive(:send_message).with(
+        { chat_id: 1, text: "No locations found." }
+      ).ordered
+
+      message2 = Telegram::Bot::Types::Message.new(text: "changi airport")
+      allow(message2).to receive(:chat).and_return(double("chat", id: 1))
+      runner.handle_message(bot, message2)
+
+      expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(SearchState)
     end
   end
 end
