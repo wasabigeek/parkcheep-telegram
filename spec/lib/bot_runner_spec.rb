d8a774f5c60e62619f9a852b44442dbb8fd14cb9
wasabigeek <wasabigeek@users.noreply.github.com>
2023-06-08 21:36:25 +0800

    Update tests

--- spec/lib/bot_runner_spec.rb
+++ spec/lib/bot_runner_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe BotRunner do
       message = Telegram::Bot::Types::Message.new(text: "/start")
       allow(message).to receive(:chat).and_return(double("chat", id: 1))
       expect(api).to receive(:send_message).with(
-        { chat_id: 1, text: "👋 Hello! Please type your destination." }
+        { chat_id: 1, text: /^👋 Where in Singapore are you going?/ }
       )
 
       runner = BotRunner.new
       runner.handle_message(bot, message)
 
-      expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(SearchState)
+      expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(NaturalSearchState)
 
       # searching, no location found
       expect_any_instance_of(Parkcheep::Geocoder).to receive(:geocode).with(
@@ -37,14 +37,15 @@ RSpec.describe BotRunner do
         { chat_id: 1, text: "Searching for \"changi airport, Singapore\"..." }
       ).ordered
       expect(api).to receive(:send_message).with(
-        { chat_id: 1, text: "No locations found." }
+        { chat_id: 1, text: "Could not find that destination on Google. Please try again with a different destination name!" }
       ).ordered
 
       message2 = Telegram::Bot::Types::Message.new(text: "changi airport")
       allow(message2).to receive(:chat).and_return(double("chat", id: 1))
       runner.handle_message(bot, message2)
 
-      expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(SearchState)
+      # this isn't quite right, but it's good enough for now
+      expect(runner.send(:retrieve_chat_state, bot, 1)).to be_a(ShowSearchDataState)
     end
   end
 end
